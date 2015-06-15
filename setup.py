e43e36c739a9cdcd1daa15063a59386ccd37996a
Lars van de Kerkhof <lars@permanentmarkers.nl>
2015-06-15 16:12:28 +0200

    Initial version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 pkgname = "vdt.versionplugin.wheel"
 
 setup(name=pkgname,
-      version="0.0.2",
+      version="0.0.1",
       description="vdt.version plugin for building python wheels.",
       author="Lars van de Kerkhof",
       author_email="lars@devopsconsulting.nl",
