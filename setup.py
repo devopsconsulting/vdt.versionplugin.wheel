0b1c34571bb291958f8d89a6bc67909778c24446
Lars van de Kerkhof <lars@permanentmarkers.nl>
2016-04-20 09:36:27 +0200

    mooi man

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 pkgname = "vdt.versionplugin.wheel"
 
 setup(name=pkgname,
-      version="0.0.4",
+      version="0.0.5",
       description="vdt.version plugin for building python wheels.",
       author="Lars van de Kerkhof",
       author_email="lars@devopsconsulting.nl",
