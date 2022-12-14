faa62162caf004b852e16b68eb30b975eccabe9c
Lars van de Kerkhof <lars@permanentmarkers.nl>
2022-12-14 12:01:28 +0100

    If setup.py contains a larger version than the tagged version take that instead

--- vdt/versionplugin/wheel/version.py
+++ vdt/versionplugin/wheel/version.py
@@ -15,6 +15,11 @@ log = logging.getLogger('vdt.versionplugin.wheel.version')
 __all__ = ('get_version')
 
 
+def get_python_version(version_args):
+    result = subprocess.check_output(['python', 'setup.py', '--version'])
+    return Version(result.decode("utf-8"), extra_args=version_args)
+
+
 def get_version(version_args):
     """
     Retrieve the version from the repo, if no version tag
@@ -23,9 +28,10 @@ def get_version(version_args):
     It can be assumed that this script will be ran in the
     root of the repository.
     """
+    python_version = get_python_version(version_args)
     try:
-        return get_git_version(version_args)
+        gitversion = get_git_version(version_args)
+        return gitversion if gitversion >= python_version else python_version
     except VersionNotFound:
         log.debug('no version tag found, taking version from setup.py')
-        result = subprocess.check_output(['python', 'setup.py', '--version'])
-        return Version(result.decode("utf-8"), extra_args=version_args)
+        return python_version
